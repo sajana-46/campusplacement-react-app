059081af523313cec7258e2352ff12d582eaceb7
sajana-46 <sajanaskuttiyil@gmail.com>
2026-07-11 12:20:16 +0530

    viewregistration completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import AddRegistration from './components/AddRegistration'
 import SearchRegistration from './components/SearchRegistration'
 import DeleteRegistration from './components/DeleteRegistration'
+import ViewRegistration from './components/ViewRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
     <AddRegistration/>
     <SearchRegistration/>
     <DeleteRegistration/>
+    <ViewRegistration/>
       
     </>
   )
